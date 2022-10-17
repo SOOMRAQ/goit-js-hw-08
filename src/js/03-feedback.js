f036fc915666e902e1ef9dc22a29ccd912f668b9
Nikita Taranenko <soomraq.x@gmail.com>
2022-10-17 16:16:25 +0300

    3 task done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+// variables
+
+const form = document.querySelector('.feedback-form');
+const button = document.querySelector('[type="submit"]');
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
+
+let data = { email: '', message: '' };
+
+// Listeners
+
+form.addEventListener(
+  'input',
+  throttle(event => {
+    //   console.log(event.target.value);
+    data[event.target.name] = event.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(data));
+  }, 500)
+);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(data);
+  localStorage.removeItem('feedback-form-state');
+  data.email = '';
+  data.message = '';
+  event.currentTarget.reset();
+});
+
+// checkStorage
+const currentData = localStorage.getItem('feedback-form-state');
+if (currentData) {
+  email.value = JSON.parse(currentData).email;
+  data.email = JSON.parse(currentData).email;
+
+  message.value = JSON.parse(currentData).message;
+  data.message = JSON.parse(currentData).message;
+}
