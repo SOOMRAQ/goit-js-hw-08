57174fd38252acc68aa8e9629e083e728f9711b0
Nikita Taranenko <soomraq.x@gmail.com>
2022-10-17 15:23:49 +0300

    2 task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+const currentTime = localStorage.getItem('your-current-time');
+
+function saveCurrentTime({ seconds }) {
+  //   console.log(seconds);
+  localStorage.setItem('your-current-time', seconds);
+}
+
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
