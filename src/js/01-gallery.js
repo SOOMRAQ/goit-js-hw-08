4e554af4d6503f12337172148f174b1828e13a70
Nikita Taranenko <soomraq.x@gmail.com>
2022-10-17 14:45:03 +0300

    1 task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+// html-structure
+
+const galleryEl = galleryItems
+  .map(({ description, original, preview }) => {
+    return `<a href="${original}" class="gallery__link">
+                <img class="gallery__image" src="${preview}" alt="${description}">
+            </a>`;
+  })
+  .join(' ');
+
+gallery.insertAdjacentHTML('beforeend', galleryEl);
+
+console.log(galleryEl);
+
+// action
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  overlayOpacity: 0.9,
+  navText: ['⇜', '⇝'],
+  closeText: '✗',
+});
